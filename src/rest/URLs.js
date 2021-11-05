2dc7879db437869a3c5ec0ab491a64870b00c964
reza <personal.rezasajadi@gmail.com>
2021-11-05 21:19:53 +0330

    Adding Rest services.

---
+++ src/rest/URLs.js
@@ -0,0 +1,7 @@
+const dogURL = process.env.REACT_APP_DOG;
+const catURL = process.env.REACT_APP_CAT;
+const foxURL = process.env.REACT_APP_FOX;
+
+export const getDogURL = () => `${dogURL}`;
+export const getCatURL = () => `${catURL}`;
+export const getFoxURL = () => `${foxURL}`;
